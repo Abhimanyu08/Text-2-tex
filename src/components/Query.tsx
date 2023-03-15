8aad3d2c8860c5af9fbb672264bbee2cec28dd8a
Abhimanyu <iamabhimanyu08@gmail.com>
2023-03-15 15:20:38 +0530

    minor improvements which should have been there in the first place lol

--- src/components/Query.tsx
+++ src/components/Query.tsx
@@ -25,23 +25,42 @@ function Query() {
 		const { latex } = await resp.json();
 		setRawLatex(latex);
 		setGettingLatex(false);
-		setQuery("");
+		// setQuery("");
 	};
 
 	return (
 		<div className="flex flex-col px-2 lg:flex-row w-full gap-3 overflow-y-auto lg:gap-0 basis-10/12  items-stretch lg:items-center lg:justify-center ">
-			<textarea
-				name=""
-				id="query"
-				value={query}
-				className="bg-black  text-white border-pink-600 border-2 
+			<div
+				className="relative
+
 				basis-3/12
+			"
+			>
+				<textarea
+					name=""
+					id="query"
+					value={query}
+					className="bg-black  text-white border-pink-600 border-2 
 				lg:h-52
+				w-full
+				h-full
 				font-mono
                     rounded-lg p-4"
-				onChange={(e) => setQuery(e.target.value)}
-				placeholder="describe your query in natural language and press right/down arrow to generate latex"
-			/>
+					onChange={(e) => setQuery(e.target.value)}
+					placeholder="describe your query in natural language and press right/down arrow to generate latex"
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							sendReq(query);
+						}
+					}}
+				/>
+				<button
+					className="absolute bottom-2 right-2 lg:right-4 bg-pink-500 py-1 text-black text-sm px-2 rounded-md"
+					onClick={() => setQuery("")}
+				>
+					Clear
+				</button>
+			</div>
 			{/* <button
 					onClick={() => sendReq(query)}
 					className="text-black p-2 rounded-md bg-cyan-400 px-4"
@@ -76,18 +95,36 @@ transition
 				`}
 				/>
 			</button>
-			<textarea
-				name=""
-				id=""
-				value={rawLatex.split("\n").join(" ")}
-				className="text-white bg-black basis-3/12 
+			<div className="relative basis-3/12">
+				<textarea
+					name=""
+					id=""
+					value={rawLatex.split("\n").join(" ")}
+					className="text-white bg-black 
+					w-full
+					h-full
+					lg:h-52
 
-				lg:h-52
 				border-2 border-violet-400 p-4 rounded-md tracking-wide font-mono
                 "
-				onChange={(e) => setRawLatex(e.target.value)}
-				placeholder="Edit the LaTeX"
-			/>
+					onChange={(e) => setRawLatex(e.target.value)}
+					placeholder="Edit the LaTeX"
+				/>
+				<button
+					className="absolute bottom-2 right-2 lg:right-4 bg-violet-500 py-1 text-black text-sm px-2 rounded-md"
+					onClick={(e) => {
+						navigator.clipboard.writeText(rawLatex);
+						const elem = e.currentTarget;
+						e.currentTarget.innerText = "Copied";
+						setTimeout(() => {
+							elem.innerText = "Copy";
+							elem.style.backgroundColor = "";
+						}, 3000);
+					}}
+				>
+					Copy
+				</button>
+			</div>
 
 			<button
 				className="text-3xl basis-1/12
